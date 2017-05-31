9620c62e6706c8fe09adaaec42d7483f5f687cc8
yong <308872563@qq.com>
2017-05-31 16:03:01 +0800

    Update BusinessScene.py

--- BusinessScene.py
+++ BusinessScene.py
@@ -1,47 +1,41 @@
-import commands
-import re
+# coding:utf-8
+
+from FunctionScene import *
 
 
-def string_to_number(string):
-    if string.startswith('0x'):
-        return int(string, 16)
-    else:
-        return int(string, 10)
+class BusinessScene:
+    def __init__(self, name):
+        self.name = name
+        self.scenes = []
+
+    def validate(self,):
+        for function_scene in self.scenes:
+            result, _ = function_scene.validate()
+            if result is not True:
+                print "%s validate failed" % function_scene.name
+                return False
+            print "%s validate success" % function_scene.name
+            print ""
+        return True
 
 
-def is_number_string(number_string):
-    return re.match('^0x[0-9a-fA-F]$', number_string) is not None or re.match('^[0-9]+$', number_string) is not None
+class VXLANScene (BusinessScene):
+    def __init__(self):
+        name = "vxlan"
+        BusinessScene.__init__(self, name)
+        arp_send = FunctionScene(name, "local", "send", "arp", "request")
+        arp_recv = FunctionScene(name, "local", "recv", "arp", "reply")
+        ip_send = FunctionScene(name, "local", "send", "ip", "request")
+        ip_recv = FunctionScene(name, "local", "recv", "ip", "reply")
+        self.scenes = [arp_send, arp_recv, ip_send, ip_recv]
+        # self.scenes = [arp_recv]
 
 
-def ip_hex_string(ip_dot_string):
-    numbers = ip_dot_string.split('.')
-    print(len(numbers))
-    if len(numbers) != 4:
-        print("error ip string: " + ip_dot_string)
-        return ""
+class DVRScene (BusinessScene):
+    def __init__(self):
+        name = "dvr"
+        BusinessScene.__init__(self, name)
+        ip_send = FunctionScene(name, "local", "send", "ip", "request")
+        ip_recv = FunctionScene(name, "local", "recv", "ip", "reply")
+        self.scenes = [ip_send, ip_recv]
 
-    hex_string = '0x'
-    for item in numbers:
-        string = "%02x" % int(item)
-        hex_string += string
-    return hex_string
-
-
-def execute_command(command):
-    result = commands.getoutput(command)
-    # print(result)
-    return result
-
-
-def ofport_of_device(device_name):
-    result = execute_command('ovs-vsctl get interface %s ofport' % device_name)
-    return int(result)
-
-
-def flow_of_index(flows, index):
-    for flow in flows:
-        if flow.table == index:
-            return flow
-
-    print 'not find table at of index:%d!' % index
-    return None
