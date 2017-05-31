3ea0a49e700effbe31da70a8eb86dd0c3a698f36
yong <308872563@qq.com>
2017-05-31 16:06:43 +0800

    Update FunctionScene.py

--- FunctionScene.py
+++ FunctionScene.py
@@ -9,15 +9,19 @@ class FunctionScene (Scene):
     def __str__(self):
         return self.name
 
-    def __init__(self, scene_name, dl_type, direction, env_type):
+    # config_type: local / remote
+    # direction: send / recv
+    # pkt_type: request / reply
+    def __init__(self, scene_name, config_type, direction, dl_type, pkt_type):
         name = "%s_%s_%s" % (scene_name, dl_type, direction)
         Scene.__init__(self, name)
         self.scene_name = scene_name
         self.dl_type = dl_type
         self.direction = direction
+        self.pkt_type = pkt_type
         self.jump_flow = Flow('')
 
-        local_config, remote_config = FunctionScene.init_config(scene_name, env_type)
+        local_config, remote_config = FunctionScene.init_config(scene_name, config_type)
         self.local_config = local_config
         self.remote_config = remote_config
 
@@ -25,11 +29,11 @@ class FunctionScene (Scene):
         self.init_flows_and_rules(name)
 
     @staticmethod
-    def init_config(scene_name, env_type):
-        if env_type == "local":
+    def init_config(scene_name, config_type):
+        if config_type == "local":
             return ConfigManager.local_config(scene_name)
 
-        if env_type == "remote":
+        if config_type == "remote":
             return ConfigManager.remote_config(scene_name)
 
     # TODO: 各个场景下的初始化属性
@@ -56,16 +60,29 @@ class FunctionScene (Scene):
         elif self.dl_type == 'ip':
             self.jump_flow.match_fields.update({'dl_type': int(0x0800), })
 
-        if self.direction == 'send':
-            if self.scene_name == 'vxlan' and self.dl_type == 'arp':
-                self.jump_flow.match_fields.update({'dst_mac': 'FF:FF:FF:FF:FF:FF'.lower(), })
-            elif self.scene_name == 'dvr':
-                self.jump_flow.match_fields.update({'dst_mac': self.local_config.vlanif10_mac, })
-        elif self.direction == 'recv':
-            if self.scene_name == 'vxlan':
-                self.jump_flow.match_fields.update({'tun_id': l2_tun_id, })
+        updated_fields = {}
+        if 'vxlan' in self.scene_name and self.dl_type == 'arp':
+            if self.direction == 'send':
+                if self.pkt_type == 'request':
+                    updated_fields.update({'dst_mac': 'FF:FF:FF:FF:FF:FF'.lower(), 'src_mac': self.local_config.vm_mac})
+                elif self.pkt_type == 'reply':
+                    updated_fields.update({'dst_mac': self.remote_config.vm_mac, 'src_mac': self.local_config.vm_mac})
+            elif self.direction == 'recv':
+                if self.pkt_type == 'request':
+                    updated_fields.update({'dst_mac': 'FF:FF:FF:FF:FF:FF'.lower(), 'src_mac': self.remote_config.vm_mac})
+                elif self.pkt_type == 'reply':
+                    updated_fields .update({'dst_mac': self.local_config.vm_mac, 'src_mac': self.remote_config.vm_mac})
+
+        if 'dvr' in self.scene_name:
+            updated_fields.update({'dst_mac': self.local_config.vlanif10_mac, })
+        
+        if self.direction == 'recv':
+            if 'vxlan' in self.scene_name:
+                updated_fields.update({'tun_id': l2_tun_id})
             else:
-                self.jump_flow.match_fields.update({'tun_id': l3_tun_id })
+                updated_fields.update({'tun_id': l3_tun_id})
+
+        self.jump_flow.match_fields.update(updated_fields)
 
     # vxlan_arp_send
     def init_flows_and_rules(self, function_name):
