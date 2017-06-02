de2530cf8485aa7464f8026edb1a5d5db1310aad
yong <308872563@qq.com>
2017-06-02 10:51:20 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,11 +1,18 @@
 # coding:utf-8
 
-l2_tun_id = '0x4e20'
-l3_tun_id = '0x4e30'
+import fileinput
+import re
+from utils import *
+
+default_bridge = 'br-int'
+l2_tun_id = 0x4e20
+# l2_tun_id = 0x24B8
+l3_tun_id = 0x4e30
 
 
 class Config(object):
     def __init__(self):
+        self.br_name = ''
         self.br_ip = ''
         self.vxlan_port = 0
         self.vm_port = 0
@@ -15,87 +22,61 @@ class Config(object):
         self.vlanif10_mac = ''
         self.vlanif100_mac = ''
 
-    @staticmethod
-    def vxlan_config1():
-        config = Config()
-        config.br_ip = '7.0.0.3'
-        config.vxlan_port = 1  # ofport_of_device
-        config.vm_port = 3  # ofport_of_device
-        config.vm_ip = '1.2.3.3'
-        config.vm_mac = '52:54:00:00:73:03'
-        return config
-
-    @staticmethod
-    def vxlan_config2():
-        config = Config()
-        config.br_ip = '7.0.0.4'
-        config.vxlan_port = 1  # ofport_of_device
-        config.vm_port = 3  # ofport_of_device
-        config.vm_ip = '1.2.3.9'
-        config.vm_mac = '52:54:00:00:74:03'
-        return config
-
-    @staticmethod
-    def dvr_config1():
-        config = Config()
-        config.br_ip = '7.0.0.3'
-        config.vxlan_port = 1  # ofport_of_device
-        config.vm_port = 3  # ofport_of_device
-        config.vm_ip = '1.2.3.3'
-        config.vm_mac = '52:54:00:00:73:03'
-        config.vlanif10_ip = '1.2.3.1'
-        config.vlanif10_mac = '52:54:01:02:03:01'
-        config.vlanif100_mac = '52:54:01:02:03:02'
-        return config
-
-    @staticmethod
-    def dvr_config2():
-        config = Config()
-        config.br_ip = '7.0.0.4'
-        config.vxlan_port = 1  # ofport_of_device
-        config.vm_port = 3  # ofport_of_device
-        config.vm_ip = '1.2.4.3'
-        config.vm_mac = '52:54:00:00:74:03'
-        config.vlanif10_ip = '1.2.4.1'
-        config.vlanif10_mac = '52:54:01:02:04:01'
-        config.vlanif100_mac = '52:54:01:02:04:02'
-        return config
-
 
 class ConfigManager(object):
-    def __init__(self):
-        pass
-
-    # scene_name = vxlan
-    @staticmethod
-    def local_config(scene_name):
-        function = ConfigManager.function_of_config_with_scene_name(scene_name, "config1")
-        if function is not None:
-            local_config = function()
-
-        function = ConfigManager.function_of_config_with_scene_name(scene_name, "config2")
-        if function is not None:
-            remote_config = function()
-
-        return local_config, remote_config
+    def __init__(self, config_file_path):
+        self.config_file = config_file_path
+        self.side_configs_dict = self.read_config_file(config_file_path)
 
     @staticmethod
-    def remote_config(scene_name):
-        function = ConfigManager.function_of_config_with_scene_name(scene_name, "config2")
-        if function is not None:
-            local_config = function()
+    def read_config_file(config_file_path):
+        configs_dict = {}
 
-        function = ConfigManager.function_of_config_with_scene_name(scene_name, "config1")
-        if function is not None:
-            remote_config = function()
+        config_name = ''
+        for line in fileinput.input(config_file_path):
+            line = line.strip()
 
-        return local_config, remote_config
+            if len(line) == 0 or line[0] == '#':
+                continue
 
-    @staticmethod
-    def function_of_config_with_scene_name(scene_name, config_name):
-        config = "%s_%s" % (scene_name, config_name)
-        function = getattr(Config, config)
-        if function is None:
-            print "Config does not have function named %s" % config
+            if re.match('^\[[a-zA-Z.]+\]$', line):
+                config_name = line[1:-1]
+                if config_name not in configs_dict:
+                    configs_dict[config_name] = {}
+                continue
 
-        return function
+            index = line.find('=')
+            if index == 0:
+                continue
+
+            key = line[0:index].strip()
+            value = line[index+1:].strip()
+            configs_dict[config_name][key] = value
+
+        side_configs = {}
+        for side, side_config_dict in configs_dict.items():
+            config = Config()
+
+            for key, value in side_config_dict.items():
+                if hasattr(config, key) is not None:
+                    if isinstance(getattr(config, key), int):
+                        setattr(config, key, string_to_number(value))
+                    else:
+                        setattr(config, key, value)
+
+            side_configs[side] = config
+
+        return side_configs
+
+    def get_config(self, scene_name, direction):
+        key = '%s.%s' % (scene_name, direction)
+        if key in self.side_configs_dict:
+            return self.side_configs_dict[key]
+        else:
+            print 'not find config:%s in configfile: %s' % (key, self.config_file)
+            return None
+
+
+if __name__ == "__main__":
+    side_configs_dict = ConfigManager.read_config_file("config.ini")
+    print side_configs_dict
